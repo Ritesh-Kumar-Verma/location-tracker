99532cced229e0fd9e007756772be8d9191c9b3f
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-07-16 18:58:21 +0530

    Login page visual done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,58 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 const asd = () => {
+  const [page, setPage] = useState("loin");
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const submitData = (e) => {
+    e.preventDefault();
 
-  const [formData , setFormData] = useState({
-    email:'',
-    username:'',
-    password:''
+    const formData = new FormData(e.target);
 
-  })
-  const submitData = (e)=>{
-    e.preventDefault()
-    
-    const formData = new FormData(e.target)
-
-    for(let i of formData.entries()){
-      console.log(i)
+    for (let i of formData.entries()) {
+      const tempformdata = {};
+      tempformdata[i[0]] = i[1];
+      setFormData(tempformdata);
     }
+    e.target.reset();
+  };
 
-    
-  }
+  const signUpPage = () => {
+    return (
+      <div className="signup-window">
+        <div className="left-signup">
+          <p>Sign Up</p>
 
+          <form action="" onSubmit={submitData}>
+            <label htmlFor="email">EMAIL</label>
+
+            <input type="text" name="email" id="email" />
+
+            <label htmlFor="username">USERNAME</label>
+
+            <input type="text" name="username" id="username" />
+
+            <label htmlFor="password">PASSWORD</label>
+
+            <input type="password" name="password" id="password" />
+
+            <input type="checkbox" name="agreement" id="agreement" />
+            <label htmlFor="agreement" id="agreement-label">
+              I AGREE TO THE <span>TERMS OF SERVICES</span> AND{" "}
+              <span>PRIVACY POLICY</span>
+            </label>
+            <br />
+
+            <button type="submit" id="left-btn-signup">
+              Sign Up
+            </button>
+            <button
+              type="button"
+              className="left-btn-login"
+              onClick={() => setPage("login")}
+            >
+              LOG IN
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  const loginPage = () => {
+    return (
+      <div className="login-window">
+        <div className="right-login">
+          <p>Login</p>
+          <form action="" onSubmit={submitData}>
+            <label htmlFor="username">USERNAME</label>
+
+            <input type="text" name="username" id="username" />
+
+            <label htmlFor="password">PASSWORD</label>
+
+            <input type="password" name="password" id="password" />
+
+            <button type="submit" className="right-btn-login">
+              LOGIN
+            </button>
+            <button
+              className="right-btn-signup"
+              onClick={() => setPage("signup")}
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="login-signup-window">
-      <div className="left-signup">
-        <p>Sign Up</p>
-        <form action="" onSubmit={submitData} >
-          <label htmlFor="email">EMAIL</label>
-          <br />
-          <input type="text" name="email" id="email" />
-          <br />
-
-          <label htmlFor="username">USERNAME</label>
-          <br />
-          <input type="text" name="username" id="username" />
-          <br />
-
-          <label htmlFor="password">PASSWORD</label>
-          <br />
-          <input type="password" name="password" id="password" />
-          <br />
-
-          <input type="checkbox" name="agreement" id="agreement" />
-          <label htmlFor="agreement" id="agreement">
-            I AGREE TO THE TERMS OF SERVICES AND PRIVACY POLICY
-          </label>
-
-          <input type="submit" value="Sign Up" className="btn-signup" />
-          <button type="button" className="btn-login">
-            LOG IN
-          </button>
-        </form>
-      </div>
-      <div className="right-login">asd</div>
+      {page === "login" ? loginPage() : signUpPage()}
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 👈 allows access from local network (e.g., 192.168.x.x)
+    port: 5173, // or any port you want
+  },
 })
